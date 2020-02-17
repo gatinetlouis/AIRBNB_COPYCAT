ac60b3e5177cee4c9cab6db690a16a14d26049cc
Amandine Legal <amandine.legal@edhec.com>
2020-02-17 19:04:35 +0100

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,28 @@
 class Review < ApplicationRecord
+  REVIEW_TYPES_VALUES = ["feedback_from_renter", "feedback_from_owner"]
   belongs_to :rental
-  belongs_to :user, through: :rental
-  belongs_to :car, through: :rental
 
   validates :title, :message, :rating, presence: true
+  validates :review_type, inclusion: { in: REVIEW_TYPES_VALUES }
+
+
+  def car
+    self.rental.car
+  end
+
+  def reviewer
+    if review_type == "feedback_from_renter"
+      self.rental.user
+    else
+      self.rental.car.user
+    end
+  end
+
+  def reviewed
+    if review_type == "feedback_from_renter"
+      self.rental.car.user
+    else
+      self.rental.user
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,169 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' end, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+  username: "mandou13",
+  password: "xxxxxxxx717oJNFJFJ",
+  phone_number: "073947594039",
+  email: "amandine.legal@me.com")
+
+User.create(
+  username: "maxtuninglover",
+  password: "xxxxxddofhFJ",
+  phone_number: "073947594039",
+  email: "maxence.mdr@me.com")
+
+User.create(
+  username: "Gatuning",
+  password: "xxijfhfoiFJFJ",
+  phone_number: "073947164039",
+  email: "gattuning@me.com")
+
+User.create(
+  username: "dortuning",
+  password: "xxxxddxxx717oJNFJFJ",
+  phone_number: "0739893967",
+  email: "dortuning@me.com")
+
+user_id = []
+User.all.each do |user|
+  user_id << user.id
+end
+
+
+Car.create(
+  brand: "Volvo",
+  model: "Volvo 850",
+  color: "Purple",
+  price_per_day: 134,
+  description: "Je vous propose une volvo 850 tuning, c'est un modèle unique. Elle a de belles jantes dorées, parfait pour un we",
+  pick_up_address: "34 rue de Strasbourg, 59000 Fallempin, France",
+  fuel_type: "Diesel",
+  automatic: true,
+  neon: true,
+  watts:  356,
+  rim_size: 450,
+  user_id: user_id.sample
+  )
+
+
+Car.create(
+  brand: "Peugeot",
+  model: "Peugeot 207 CC",
+  color: "Green Apple",
+  price_per_day: 155,
+  description: "Je vous propose ma belle titine, elle est pimpée à mort, avec  un super système son",
+  pick_up_address: "34 rue du Tuninf, 59080 Hemmes, France",
+  fuel_type: "Gasoil",
+  automatic: true,
+  neon: true,
+  watts:  500,
+  rim_size: 650,
+  user_id: user_id.sample
+  )
+
+Car.create(
+  brand: "Opel",
+  model: "Opel Astra 2.0",
+  color: "Yellow",
+  price_per_day: 220,
+  description: "Magnifique Opel Astra, volant en fourrure,  néon à gogo, vous allez vous régaler ",
+  pick_up_address: "55 bd Saint Etienne, 59070 Roubaix, France",
+  fuel_type: "Diesel",
+  automatic: false,
+  neon: true,
+  watts:  356,
+  rim_size: 450,
+  user_id: user_id.sample
+  )
+
+Car.create(
+  brand: "Mercedes",
+  model: "SL 350",
+  color: "Orange",
+  price_per_day: 500,
+  description: "Ancien taxi, j'ai retappé ma Merco avec beaucoup d'amour, cuir croco, sono 500db et quelques détails bling bling",
+  pick_up_address: "34 rue de Strasbourg, 59000 Fallempin, France",
+  fuel_type: "Gasoil",
+  automatic: false,
+  neon: true,
+  watts:  500,
+  rim_size: 200,
+  user_id: user_id.sample
+  )
+
+car_id = []
+Car.all.each do |user|
+  car_id << user.id
+end
+
+
+Rental.create(
+  car_id: car_id.sample,
+  user_id: user_id.sample,
+  start_date: '21/01/20',
+  end_date: '23/01/20',
+  )
+
+Rental.create(
+  car_id: car_id.sample,
+  user_id: user_id.sample,
+  start_date: '25/01/20',
+  end_date: '27/01/20'
+  )
+
+Rental.create(
+  car_id: car_id.sample,
+  user_id: user_id.sample,
+  start_date: '01/01/20',
+  end_date: '04/01/20'
+  )
+
+Rental.create(
+  car_id: car_id.sample,
+  user_id: user_id.sample,
+  start_date: '01/02/20',
+  end_date: '10/02/20'
+  )
+
+rental_id = []
+Rental.all.each do |user|
+  rental_id << user.id
+end
+
+Review.create(
+  rental_id: rental_id.sample,
+  title: "Super bolide",
+  review_type: "feedback_from_renter",
+  message: "Superbe we avec ma fem dans cette belle voiturrr tunaié",
+  rating: 5
+)
+
+Review.create(
+  rental_id: rental_id.sample,
+  title: "Décu du système son",
+  review_type: "feedback_from_renter",
+  message: "Le système son était pas ouf, mais les LED relevaient le niveau",
+  rating: 3
+)
+
+Review.create(
+  rental_id: rental_id.sample,
+  title: "Pb de moteur",
+  review_type: "feedback_from_renter",
+  message: "La voiture nous a laché au milieu du week-end.. La grosse galère",
+  rating: 1
+)
+
+Review.create(
+  rental_id: rental_id.sample,
+  title: "feedback_from_owner",
+  review_type: "Un locataire très sale",
+  message: "Des déchêts plein la voiture....",
+  rating: 1
+)
+
