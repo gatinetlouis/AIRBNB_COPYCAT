ef6cddd01188028d7f5ed576587366bbb99a85e3
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-17 15:51:35 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+
+  devise_for :users
+  resources :profiles, only: [:show]
+
+
+  resources :cars do
+    resources :rentals, only: [:new, :create]
+    resources :reviews, only: [:index, :show]
+  end
+  resources :rentals, only: [:index, :show, :destroy] do
+    resources :reviews, only: [:new, :create, :destroy]
+  end
+
+
+  # resources :reviews, only: [:index, :show]
+
+    # resources :rentals, only: [:index, :new, :create, :show, :destroy] do
+      # resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+    # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
