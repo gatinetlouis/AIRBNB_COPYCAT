422bac8eb4bbeac6c8291bb5578eef39810f3d08
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-20 16:27:30 +0100

    perfect navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
-import "bootstrap";
 import "../plugins/flatpickr"
 
 import { loadDynamicHomeText } from '../components/typed';
 import { fetchMap } from '../components/map';
 import { activitiesCollapse } from '../components/init_activities';
+import "bootstrap";
 
 
 loadDynamicHomeText();
